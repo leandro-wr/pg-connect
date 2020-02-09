f79af837c17d105553a0e411476232bbc90c8b6a
leandro-wr <leandro21wr@gmail.com>
2020-02-09 09:13:44 -0300

    começando a criar minhas migrations

--- src/database/migrations/20200201140455-create-devs.js
+++ src/database/migrations/20200201140455-create-devs.js
@@ -2,22 +2,17 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    return queryInterface.createTable('devs', { 
+      id: {
+        type: Sequelize.INTEGER,
+        
+      } 
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    });
+    
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('devs');
   }
 };
